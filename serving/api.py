1842d78f4ee5336f73092cb28fa18c525c446168
Dungnt <tiendung021004@gmail.com>
2025-11-16 00:07:31 +0700

    feat: add model serving

--- serving/api.py
+++ serving/api.py
@@ -19,8 +19,13 @@ model.eval()
 
 
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.ToTensor()
+    transforms.Resize((512, 384)),
+    transforms.CenterCrop((384, 384)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225],
+    ),
 ])
 
 
@@ -29,9 +34,12 @@ async def predict(file: UploadFile = File(...)):
     img_bytes = await file.read()
     img = Image.open(io.BytesIO(img_bytes)).convert("RGB")
 
-    x = transform(img).unsqueeze(0)  # [1, 3, 224, 224]
+    x = transform(img).unsqueeze(0)  # [1, 3, 512, 384]
     with torch.no_grad():
-        score = model(x).item()  # IQA score
+        pred = model(x)  # IQA score
+    
+    pred = pred.squeeze().item()
+    score = pred * 100.0
 
     return {"quality": score}
 
