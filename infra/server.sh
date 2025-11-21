42b322ecd1ea5826c55aaff3df2bcd06a76f7167
Dungnt <tiendung021004@gmail.com>
2025-11-21 18:43:43 +0700

    setup: configure server.sh with correct MLflow env vars

--- infra/server.sh
+++ infra/server.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Host check
 export MLFLOW_SERVER_ALLOWED_HOSTS=$MLFLOW_ALLOWED_HOSTS
-export MLFLOW_SERVER_FILE_STORE=$POSTGRESQL_URL
-export MLFLOW_SERVER_SERVE_ARTIFACTS="true"
-export MLFLOW_SERVER_ARTIFACT_DESTINATION=$STORAGE_URL
-export MLFLOW_SERVER_ARTIFACT_ROOT=$STORAGE_URL
+
+# Backend Store: Cloud SQL
+export MLFLOW_BACKEND_STORE_URI=$POSTGRESQL_URL
+
+# Artifact Store: GCS
+export MLFLOW_DEFAULT_ARTIFACT_ROOT=$STORAGE_URL
+
+# Basic auth for login
 export WSGI_AUTH_CREDENTIALS="${MLFLOW_ID}:${MLFLOW_PASS}"
 
+# Start MLflow server using Gunicorn WSGI server
 exec gunicorn -b "0.0.0.0:5000" wsgi_server:app
